37c048e734c0eaeb5ea20bb4e4773e0d7df87ca5
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-17 00:00:51 +0530

    Update stats.py

--- Banword/modules/stats.py
+++ Banword/modules/stats.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-from Banword import Banword as app
+from Banword import app
 from Banword.helper.database import get_users, get_chats, get_new_users, get_new_chats
 
 
