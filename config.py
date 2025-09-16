ec059008694f2a336b22da1ab9171c08a24d6bb0
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-17 00:24:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,17 +9,17 @@ API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 # ------------------------------------------------
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "aaradhyavidebot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "VegitoEditGurdian_Bot")
 
 # -----------------------------------------------
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "8056154987"))
-SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8056154987"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7694170809"))
+SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "7694170809"))
 # ------------------------------------------------
 
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", "-100"))
-OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "-100"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1003065367480"))
+OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "-1003065367480"))
 
 # ------------------------------------------------
 
